0e25bbb1089142c0baa6e4eb9623d690fe90f698
anmutu <anmutu@hotmail.com>
2020-02-18 02:09:18 +0800

    scheduler大概代码架构。

---
+++ scheduler/main.go
@@ -0,0 +1,9 @@
+/*
+  author='du'
+  date='2020/2/18 2:05'
+*/
+package main
+
+func main() {
+
+}
---
+++ scheduler/taskrunner/runner.go
@@ -0,0 +1,9 @@
+/*
+  author='du'
+  date='2020/2/18 2:08'
+*/
+package main
+
+func main() {
+
+}
