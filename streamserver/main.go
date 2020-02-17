392699f2d5ca67a8bb791b9a44755a29616abb09
anmutu <anmutu@hotmail.com>
2020-02-17 17:29:16 +0800

    在http midleware中嵌入流控的部分

--- streamserver/main.go
+++ streamserver/main.go
@@ -11,11 +11,33 @@ import (
 
 func main() {
 	r := RegisterHandlers()
-	http.ListenAndServe(":9000", r)
+	mh := NewMiddleWareHandler(r, 2)
+	http.ListenAndServe(":9000", mh)
 }
 
+//结构体
 type middleWareHandler struct {
-	r *httprouter.Handle
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+//hijack
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//panic("implement me")
+	if !m.l.GetConn() {
+		SendErrorResponse(w, http.StatusTooManyRequests, "请求太多")
+		return
+	}
+	m.r.ServeHTTP(w, r)
+	defer m.l.RealseConn()
+}
+
+//构造函数
+func NewMiddleWareHandler(r *httprouter.Router, count int) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(count)
+	return m
 }
 
 func RegisterHandlers() *httprouter.Router {
